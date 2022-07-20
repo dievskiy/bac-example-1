7e8b72eb8d76c7855d568ecdddfb2002870aa49f
dievskiy <potekhin.ruslan@gmail.com>
2022-07-20 14:19:50 +0300

    Add some files for test
    Change-Id: Ib1a338b1c9612bd69e96e13fa02c84d832c0e15e

---
+++ controllers/new.js
@@ -0,0 +1,9 @@
+here are some updates!
+    nsdoifnisdo
+nidsfonsdoif nsdoifnisdosndifosndio
+nsdoifnisdonsiodfnsodif
+Actually this line should be like that
+Here should be some changes as well
+nsdoifnisdo
+nsdoifnisdo
+
---
+++ controllers/test.js
@@ -0,0 +1,12 @@
+nsdifoosd fsdf
+sdfnsdifsd
+fsdffsd
+fsdffsddsf
+sdfnsdifsdf
+sdfnsdifsdfa
+fsa
+fsasadf
+dsa
+ewr
+ewrr
+
